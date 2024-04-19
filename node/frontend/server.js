148a7e9c33de4d08e7b843e06d1f201bdd954bc7
Vladimir Kähri <info@hurtlocker.pro>
2024-04-19 09:57:08 +0300

    server added

--- node/frontend/server.js
+++ node/frontend/server.js
@@ -3,19 +3,18 @@ import {createServer} from 'http'
 import fs from 'fs';
 import path from 'path';
 
+var data = fs.readFileSync('index.html');
+
 const server = createServer((request, response) => {
-    let content = fetch('./index.html')
-    fs.readFile('index.html', (err, data) => {
         // Set the response header to indicate HTML content
-        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         // Send the contents of index.html file as the response
         response.end(data);
-    });
 
     //response.writeHead(200, {'Content-type':'text/html; charset=utf-8'})
     //response.end(content)
 })
 
-server.listen(80, () => {
+server.listen(8080, () => {
     console.log("Server is running");
 })
